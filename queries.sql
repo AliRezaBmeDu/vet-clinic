5a9956faabb790741f7a21491aaaf14fa1f7a82a
AliReza <ali.reza.buet@gmail.com>
2023-09-26 02:58:58 +0600

    Select specific commands for specific queries

--- queries.sql
+++ queries.sql
@@ -46,3 +46,22 @@ ROLLBACK TO weight_update_savepoint;
 UPDATE animals SET weight_kg = -1 * weight_kg WHERE weight_kg < 0;
 
 COMMIT;
+
+/* quries */
+/* How many animals are there? */
+SELECT COUNT(*) AS total_animals FROM animals;
+
+/* How many animals have never tried to escape? */
+SELECT COUNT(*) AS never_tried_to_escape FROM animals WHERE escape_attempts = 0;
+
+/* What is the average weight of animals? */
+SELECT AVG(weight_kg) AS average_weight FROM animals;
+
+/* Who escapes the most, neutered or not neutered animals? */
+SELECT neutered, MAX(escape_attempts) AS max_escape_attempts FROM animals GROUP BY neutered;
+
+/* What is the minimum and maximum weight of each type of animal? */
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight FROM animals GROUP BY species;
+
+/* What is the average number of escape attempts per animal type of those born between 1990 and 2000? */
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
