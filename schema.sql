5c998b08015dcdd87889d157e3a93baea40c292a
AliReza <ali.reza.buet@gmail.com>
2023-09-27 04:51:12 +0600

    Add commands for: create vet table, many-to-many relationship tables specializations and visits

--- schema.sql
+++ schema.sql
@@ -54,3 +54,27 @@ ADD CONSTRAINT fk_species_id FOREIGN KEY (species_id) REFERENCES species(id);
 -- Add the "owner_id" column as a foreign key referencing the "owners" table:
 ALTER TABLE animals ADD COLUMN owner_id INTEGER,
 ADD CONSTRAINT fk_owner_id FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+
+-- create new table named vets
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    age INTEGER,
+    date_of_graduation DATE
+);
+
+-- many-to-many relationship between vets and species
+CREATE TABLE specializations (
+    vet_id INTEGER REFERENCES vets(id),
+    species_id INTEGER REFERENCES species(id),
+    PRIMARY KEY (vet_id, species_id)
+);
+
+-- many-to-many relationship between vets and species
+CREATE TABLE visits (
+    vet_id INTEGER REFERENCES vets(id),
+    animal_id INTEGER REFERENCES animals(id),
+    visit_date DATE,
+    PRIMARY KEY (vet_id, animal_id, visit_date)
+);
