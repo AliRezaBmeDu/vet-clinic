0cf573de6632f019b34f2a33d83ab098e959a064
AliReza <ali.reza.buet@gmail.com>
2023-09-27 01:33:08 +0600

    Add commands for updating the owner_id column

--- data.sql
+++ data.sql
@@ -39,3 +39,13 @@ SET species_id = CASE
 WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
 ELSE (SELECT id FROM species WHERE name = 'Pokemon')
 END;
+
+-- Update the "owner_id" column based on owner information
+UPDATE animals
+SET owner_id = CASE
+WHEN name = 'Agumon' THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+WHEN name IN ('Gabumon', 'Pikachu') THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+WHEN name IN ('Devimon', 'Plantmon') THEN (SELECT id FROM owners WHERE full_name = 'Bob')
+WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+WHEN name IN ('Angemon', 'Boarmon') THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+END;
