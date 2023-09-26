1e929cfbdd03bd8ffc7afa0680c89c3e88c86d77
AliReza <ali.reza.buet@gmail.com>
2023-09-27 01:31:16 +0600

    Insert new data to owners and species

--- data.sql
+++ data.sql
@@ -16,3 +16,19 @@ INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, 
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species) VALUES ('Boarmon', '2005-06-07', 20.4, true, 7, NULL);
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species) VALUES ('Blossom', '1998-10-13', 17, true, 3, NULL);
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species) VALUES ('Ditto', '2022-05-14', 22, true, 4, NULL);
+
+-- Insert data into the "owners" table
+INSERT INTO owners (full_name, age)
+VALUES
+    ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
+	
+-- Insert data into the "species" table
+INSERT INTO species (name)
+VALUES
+    ('Pokemon'),
+    ('Digimon');
